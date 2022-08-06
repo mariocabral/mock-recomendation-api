8b3be2c30c7f05164a93e124bc7ef15220558e3e
Mario Cabral <mariocabral.dev@gmail.com>
2022-08-06 01:19:36 -0300

    fix code style

--- mock_data/mock_recommendation.py
+++ mock_data/mock_recommendation.py
@@ -3,22 +3,21 @@ from mock_data.data.products import products_by_store_and_subsidiary
 from mock_data.data.shopping_cart import shopping_cart_by_store_and_subsidiary
 
 
-
 def get_product_recommendation(store_id, subsidiary_id, product):
     print(f'Find recommendation of store "{store_id}" subsidiary "{subsidiary_id}" product "{product}"')
-    for r in products_by_store_and_subsidiary:
-        print(f'Check store "{r["storeId"]}" subsidiary "{r["subsidiaryId"]}" product "{r["external"]}"')
-        if r["storeId"] == store_id and r["subsidiaryId"] == subsidiary_id and r["external"] == product:
+    for row in products_by_store_and_subsidiary:
+        print(f'Check store "{row["storeId"]}" subsidiary "{row["subsidiaryId"]}" product "{row["external"]}"')
+        if row["storeId"] == store_id and row["subsidiaryId"] == subsidiary_id and row["external"] == product:
             print('Recommendation found!')
-            return json.dumps(r["recommendation"])
+            return json.dumps(row["recommendation"])
     return json.dumps([])
 
 
 def get_shopping_cart_recommendation(store_id, subsidiary_id, items):
     print(f'Find recommendation of store "{store_id}" subsidiary "{subsidiary_id}" product "{items}"')
-    for r in shopping_cart_by_store_and_subsidiary:
-        print(f'Check store "{r["storeId"]}" subsidiary "{r["subsidiaryId"]}"')
-        if r["storeId"] == store_id and r["subsidiaryId"] == subsidiary_id:
+    for row in shopping_cart_by_store_and_subsidiary:
+        print(f'Check store "{row["storeId"]}" subsidiary "{row["subsidiaryId"]}"')
+        if row["storeId"] == store_id and row["subsidiaryId"] == subsidiary_id:
             print('Recommendation found!')
-            return json.dumps(r["recommendation"])
+            return json.dumps(row["recommendation"])
     return json.dumps([])
