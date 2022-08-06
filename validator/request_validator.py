be9a20b51b1cf13c31e168e47f26969dc7b22164
Mario Cabral <mariocabral.dev@gmail.com>
2022-08-06 01:20:54 -0300

    fix code style

--- validator/request_validator.py
+++ validator/request_validator.py
@@ -2,13 +2,13 @@ from functools import wraps
 from flask import request, abort
 
 
-def validate(f):
-    @wraps(f)
+def validate(function_call):
+    @wraps(function_call)
     def decorated(*args, **kwargs):
         application = request.headers.get('x-application')
         platform = request.headers.get('x-platform')
         if application == 'CUS' and platform == 'AND':
-            return f(*args, **kwargs)
+            return function_call(*args, **kwargs)
         return abort(403)
 
     return decorated
